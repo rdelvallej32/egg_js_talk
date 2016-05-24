b02dfa6484f25b2d77060cc967efe3d41aed1593
rdelvallej32 <rdelvallej@gmail.com>
2016-05-24 14:17:26 -0400

    Add document on ready handler
    - Require event handler from easter-egg file
    - Add jquery to listen for easter eggs on document ready.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -3,5 +3,11 @@
 // user require with a reference to bundle the file and use it in this file
 // var example = require('./example');
 
+const eventHandlers = require('./easter_eggs.js');
 // use require without a reference to ensure a file is bundled
-require('./example');
+// require('./example');
+
+
+$(() => {
+  eventHandlers.addHandlers();
+});
