64bdc4f13083ac08a757802a14538d43f0d5bbd8
rdelvallej32 <rdelvallej@gmail.com>
2016-05-25 08:39:09 -0400

    Remove non-working code
    - Removed nav easter egg code for purposes of demonstration

--- assets/scripts/easter_eggs.js
+++ assets/scripts/easter_eggs.js
@@ -3,12 +3,6 @@
 const addHandlers = function() {
   let egg = new Egg();
 
-
-  egg.addCode("n,a,v", function() {
-    $('.navbar-nav').append("<li><a href='#'>Home</a></li>");
-
-  }).listen();
-
   ///example for the class
   egg.addCode("up,up,down,down,left,right,left,right,b,a", function() {
     jQuery('#egggif').fadeIn(500, function() {
